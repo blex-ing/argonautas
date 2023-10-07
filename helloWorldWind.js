32591654e8c1ed49231ad4fab44b6e1905d4103a
Michael Espinosa Males <47524846+blex-ing@users.noreply.github.com>
2023-10-07 14:57:16 +0000

    add more data

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -4,3 +4,10 @@ wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+var wwd1 = new WorldWind.WorldWindow("canvasTwo");
+wwd1.addLayer(new WorldWind.BMNGOneImageLayer());
+wwd1.addLayer(new WorldWind.BMNGLandsatLayer());
+wwd1.addLayer(new WorldWind.CompassLayer());
+wwd1.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd1));
+wwd1.addLayer(new WorldWind.ViewControlsLayer(wwd2));
